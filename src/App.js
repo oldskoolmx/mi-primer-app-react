0987bd233741dfbcc178400f4606dd97511d6478
moon <merolmen@gmail.com>
2023-08-25 23:02:25 -0600

    mi primer app en react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Contador from './components/Contador';
+
+
+function App() {
+  return (
+    <div>
+    <h1>Hola mundo react </h1>
+    <Contador />
+     
+    </div>
+  );
+}
+
+export default App;
